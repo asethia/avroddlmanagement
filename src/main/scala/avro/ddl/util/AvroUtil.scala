da1a2aff7c1e37cb9bf37142ab9a7d7b3d0f4d9c
Arun Sethia <arun.sethia@metrosystems-external.net>
2017-12-25 22:14:14 +0530

    added avro util functionss

--- src/main/scala/avro/ddl/util/AvroUtil.scala
+++ src/main/scala/avro/ddl/util/AvroUtil.scala
@@ -3,15 +3,49 @@ package avro.ddl.util
 import avro.ddl.types.CommonDataTypes._
 import com.sksamuel.avro4s.json.JsonToAvroConverter
 import org.apache.avro.Schema
+import org.json4s._
 
 import scala.util.{Success, Try}
 
 /**
-  * this is avro Utlility Class
+  * this is avro Utility Class
   * provides basic avro related utility code
   * Created by arun_sethia on 23/12/17.
   */
 trait AvroUtil {
+
+  import avro.ddl._
+
+  /**
+    * traverse Json Tree for given schema
+    *
+    * @param schema
+    */
+  def traverseTree(schema: Schema) = {
+
+
+  }
+
+  /**
+    * find root node
+    *
+    * @param schema
+    * @return
+    */
+  private def findRootNode(schema: Schema): List[Map[String, Any]] = {
+
+    val elements: Map[String, Any] = schema.elements
+
+    //root name of schema
+    val rootName: JsonData = elements.elementVal("name", "dana")
+
+    //root name space of elements
+    val rootNs: JsonData = elements.elementVal("namespace", "mcc.dana.json")
+
+    //find root level fields
+    elements.elementVal("fields", List.empty[Map[String, Any]])
+  }
+
 
   /**
     * this will generate schema for given Json data, namespace and schema name
